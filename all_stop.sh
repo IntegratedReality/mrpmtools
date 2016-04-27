eaa52368c2f204ac1f972f16aede8ecaf89a3971
Taro Seimitsu <pe2015mayfestival@gmail.com>
2016-04-27 03:14:45 +0000

    add test motor sh

---
+++ all_stop.sh
@@ -0,0 +1,5 @@
+gpio -g write 17 0
+gpio -g write 18 0
+gpio -g write 22 0
+gpio -g write 27 0
+gpio -g write 24 0
---
+++ go_f3s.sh
@@ -0,0 +1,5 @@
+gpio -g write 17 1
+gpio -g write 27 1
+sleep 3
+gpio -g write 17 0
+gpio -g write 27 0
